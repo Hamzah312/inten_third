bda17f89ccb51ee7c11cd9fc66e509e1d8f39c46
Hamzah312 <64075040+Hamzah312@users.noreply.github.com>
2023-08-31 10:34:43 +0300

    fix bugs 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CountriesListComponent } from './components/countries-list/countries-list.component';
 import { MatTableModule } from '@angular/material/table';
@@ -19,7 +18,6 @@ import { MatDialogModule } from '@angular/material/dialog';
   declarations: [AppComponent, CountriesListComponent, BordersTableComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
